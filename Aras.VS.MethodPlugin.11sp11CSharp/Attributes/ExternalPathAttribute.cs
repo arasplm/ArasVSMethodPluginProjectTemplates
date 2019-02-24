30e6e537e88408b259e2a272f5fe9009daa70415
Volodymyr Afanasiev <s-vafanasiev@aras.com>
2019-02-24 15:19:56 +0200

    Added "ExternalPathAttribute" to code templates

---
+++ Aras.VS.MethodPlugin.11sp11CSharp/Attributes/ExternalPathAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Common.Attributes
+{
+	[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum)]
+	public sealed class ExternalPathAttribute : Attribute
+	{
+		private readonly string path;
+
+		public string Path
+		{
+			get { return path; }
+		}
+
+		public ExternalPathAttribute(string path)
+		{
+			this.path = path;
+		}
+	}
+}
---
+++ Aras.VS.MethodPlugin.11sp12CSharp/Attributes/PartialPathAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Common.Attributes
+{
+	[AttributeUsage(AttributeTargets.All)]
+	public sealed class PartialPathAttribute : Attribute
+	{
+		private readonly string path;
+
+		public string Path
+		{
+			get { return path; }
+		}
+
+		public PartialPathAttribute(string path)
+		{
+			this.path = path;
+		}
+	}
+}
